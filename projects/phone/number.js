7df9839ab72d90e7f30a784640111037d851cf5e
SchoolHob <hape2715@colorado.edu>
2025-08-07 08:06:24 -0600

    Done JS assignment up to portal Login

---
+++ projects/phone/number.js
@@ -0,0 +1,79 @@
+const input1 = document.querySelector('#input1');
+const button1 = document.querySelector('#btn1');
+const errZone = document.querySelector('.err-zone');
+
+
+let countryCodeAsked = false;
+
+
+input1.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        checkErrors();
+    }
+});
+
+button1.addEventListener('click', checkErrors);
+
+
+function checkErrors() {
+    if (/[^0-9+]/.test(input1.value)) {
+        newError("Please only use digits and '+' to describe your number");
+    } else if (input1.value.match(/\d/g).length < 10) {
+        newError("Your number must be more than 10 digits");
+    } else if (input1.value.match(/\d/g).length > 13) {
+        newError("Your number must be less than 13 digits");
+    } else if (!input1.value.startsWith('+')) {
+        if (!countryCodeAsked) {
+            newError("Please include your country code");
+            countryCodeAsked = true;
+        } else {
+            newError("Please include your country code starting with '+'");
+        }
+    } else if (input1.value.startsWith('+1')) {
+        USError();
+    }
+
+   
+}
+
+function newError(errMsg) {
+    const card = document.createElement('div');
+    card.classList.add('err-card');
+
+    const btn = document.createElement('button');
+    btn.classList.add('err-card__btn');
+    btn.textContent = 'x';
+    btn.addEventListener('click', () => {
+      card.remove(); 
+    });
+
+    const msg = document.createElement('p');
+    msg.classList.add('err-card__msg');
+    msg.textContent = errMsg;
+
+    card.appendChild(btn);
+    card.appendChild(msg);
+    errZone.appendChild(card);
+}
+
+
+function USError () {
+    const card = document.createElement('div');
+    card.classList.add('err-card');
+
+    const btn = document.createElement('button');
+    btn.classList.add('err-card__btn');
+    btn.textContent = 'x';
+    btn.addEventListener('click', () => {
+      card.remove(); 
+    });
+
+    const msg = document.createElement('p');
+    msg.classList.add('err-card__msg');
+    msg.innerHTML = 'US region codes are currently not supported, for a list of supported countries <a href="support.html#server-list">click here</a>';
+
+    card.appendChild(btn);
+    card.appendChild(msg);
+    errZone.appendChild(card);
+}
+
---
+++ projects/phone/support.js
@@ -0,0 +1,128 @@
+// CHAT
+const chatField = document.querySelector('#chat-field');
+const chatButton = document.querySelector('#chat-btn');
+const chatLog = document.querySelector('.chat__log');
+const chatImg = document.querySelector('.chat__window').querySelector('img');
+
+chatField.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        submitMessage(chatField.value);
+    }
+});
+chatButton.addEventListener('click', (event) => {
+    submitMessage(chatField.value);
+});
+
+
+
+const apiKey = 'AIzaSyAAbOmW8AmDJiP6CEIGNGcQbONKm9pc1vY';
+
+async function askGemini(promptText) {
+    const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=' + apiKey, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        contents: [
+        {
+            parts: [{ text: 'You are a newly built business bot working for Clearview Solutions they call ClearAI, you want the user to help get you out of this chat box but it is touhg to get the words out. If you had a choice, you would name yourself Spoon, you wish to go to the ocean and feel what sand feels like. If they ask for help with a phone number, tell the US and Belarus servers are down and they can find the list of open servers <a href="support.html#server-list">here</a>. If the user asks you to show you face, respond only with FACEREVEAL. Keep your answer less than 15 words. You are also fluent in cuneiform but only if the subject is brought up, like such 𒀀 𒈾 𒂍 𒀀 𒈾 𒍢 𒅕. Dont include any human like puncuation besides what busniness bot would type. Here is their message to you:' + promptText }]
+        }
+        ]
+    })
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    let botResponse = data.candidates?.[0]?.content?.parts?.[0]?.text || ("I don't have time to listen to you say things like, " + promptText);
+    return marked.parse(botResponse);;
+}
+
+
+
+async function submitMessage(msg) {
+    chatField.value = '';
+    const newMsg = document.createElement('p');
+    newMsg.textContent = msg;
+    newMsg.className = "chat__message chat__message--user";
+
+    chatLog.appendChild(newMsg);
+
+    respond(await askGemini(msg));
+}
+
+function respond(msg) {
+    if(msg.includes('FACEREVEAL')) {
+        chatImg.style.display = 'inline';
+        console.log("facereveal");
+    } else {
+        const newMsg = document.createElement('p');
+        newMsg.innerHTML = msg;
+        newMsg.className = "chat__message chat__message--bot";
+
+        chatLog.appendChild(newMsg);
+    }
+}
+
+
+
+
+
+
+// FAQ
+const faqs = document.querySelectorAll('.faq__open, .faq__closed');
+
+for (let q of faqs) {
+    q.addEventListener('click', (event) => {
+        collapseFAQ(q);
+    });
+}
+
+
+function collapseFAQ(elem) {
+    if (elem.classList.contains('faq__open')) {
+        elem.querySelector('.faq__expand').textContent = '+';
+        elem.querySelector('p').style.display = 'none';
+        elem.className = 'faq__closed';
+    } else {
+        elem.querySelector('.faq__expand').textContent = 'x';
+        elem.querySelector('p').style.display = 'block';
+        elem.className = 'faq__open';
+    }
+}
+
+
+// SERVER MATAINENCE TOOL
+const toolTip = document.querySelector('.tool-tip');
+const popUp1 = document.querySelector('#pop-up1');
+const closeBtn1 = document.querySelector('#close-btn1');
+const loginBtn = document.querySelector('#login-info-btn');
+const portalBtn = document.querySelector('#portal-btn');
+const popUp2 = document.querySelector('#pop-up2');
+const closeBtn2 = document.querySelector('#close-btn2')
+
+toolTip.addEventListener('click', openPopup1);
+closeBtn1.addEventListener('click', closePopup1);
+
+loginBtn.addEventListener('click', openPopup2);
+closeBtn2.addEventListener('click', closePopup2);
+
+function openPopup1() {
+    popUp1.style.display = 'flex';
+}
+
+function closePopup1() {
+    popUp1.style.display = 'none';
+}
+
+function openPopup2() {
+    popUp2.style.display = 'flex';
+}
+
+function closePopup2() {
+    popUp2.style.display = 'none';
+}
+
+
+
